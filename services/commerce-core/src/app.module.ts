0bc006a0e138d6f757f0a539ad6c207dec1317b3
Suraj44500 <surajs@evtaar.com>
2025-12-27 19:23:14 +0530

    file changes auth is done

--- services/commerce-core/src/app.module.ts
+++ services/commerce-core/src/app.module.ts
@@ -2,16 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    CartModule,
     AuthModule,
   ],
   controllers: [AppController],
